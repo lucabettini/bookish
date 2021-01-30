7b4952804214ba73123f04eacfeaaa74f9dc0f68
lucabettini <bettiniluca1@gmail.com>
2021-01-30 23:52:51 +0100

    Solved query bug

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -9,17 +9,15 @@ import { Form, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 const Searchbar = () => {
   const [text, setText] = useState('');
-  const [query, setQuery] = useState(false);
+  const [query, setQuery] = useState(null);
   const { dispatch } = useContext(booksContext);
   const history = useHistory();
 
   useEffect(() => {
     async function fetchData() {
-      if (query) {
-        // Redirect user to /books route (Header + ResultsScreen components)
-        history.push('/books');
-        dispatch({ type: SET_STATUS, payload: { status: 'loading' } });
-      }
+      // Redirect user to /books route (Header + ResultsScreen components)
+      history.push('/books');
+      dispatch({ type: SET_STATUS, payload: { status: 'loading' } });
       try {
         const res = await fetchBooks(query);
         dispatch({ type: GET_BOOKS, payload: { books: res } });
@@ -27,9 +25,12 @@ const Searchbar = () => {
       } catch (err) {
         dispatch({ type: SET_STATUS, payload: { status: 'failed' } });
         dispatch({ type: GET_BOOKS, payload: { books: [] } });
+        localStorage.clear();
       }
     }
-    fetchData();
+    if (typeof query === 'string') {
+      fetchData();
+    }
     // We can include the dispatch function in the dependecy array since it
     // remains constant by default
   }, [query, dispatch, history]);
